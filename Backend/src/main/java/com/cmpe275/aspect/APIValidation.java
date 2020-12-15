3b125292ecf613705bf007f0d606c2365a47a8e7
bhavana <bhavanaobellaneni@gmail.com>
2020-12-14 23:24:11 -0800

    aspect validation

--- Backend/src/main/java/com/cmpe275/aspect/APIValidation.java
+++ Backend/src/main/java/com/cmpe275/aspect/APIValidation.java
@@ -8,56 +8,105 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cmpe275.Exception.CustomException;
 import com.cmpe275.Exception.ValidationException;
 import com.cmpe275.entity.Offer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.cmpe275.entity.Enum;
 
 @Aspect
 @Order(1)
 @Component
 public class APIValidation {
 	
-//    checking the required parameters for createPlayer
-	
-//	public void ValidateCreatePlayerAPI(JoinPoint joinPoint, HttpServletRequest request) {
-//		if (request.getParameter("username") == null || request.getParameter("username") == "") {
-//			throw new ValidationException("Email is missing which is required parameter.");
-//		}
-//		if (request.getParameter("nickname") == null || request.getParameter("nickname") == "") {
-//			throw new ValidationException("nickname is missing which is required parameter.");
-//		}
-//		
-//	}
-//	
-	
 	@Before("execution(public * com.cmpe275.controller.ExchangeRateController.createOffer(..))")
-	public void ValidateExchangeOffer(JoinPoint joinPoint) throws CustomException {
-	   
+	public void ValidateExchangeOffer(JoinPoint joinPoint ) {
+		JsonNode body = (JsonNode) joinPoint.getArgs()[1];
+		if (body.get("destinationCountry") == null || body.get("destinationCountry").asText() == "") {
+			throw new ValidationException("Destination Country should not be null");
+		}
+		if (body.get("destinationCurrency") == null || body.get("destinationCurrency").asText() == "") {
+			throw new ValidationException("Destination Currency should not be null");
+		}
+		if (body.get("sourceCountry") == null || body.get("sourceCountry").asText() == "") {
+			throw new ValidationException("Source Country should not be null");
+		}
+		if (body.get("sourceCurrency") == null || body.get("sourceCurrency").asText() == "") {
+			throw new ValidationException("Source Currency should not be null");
+		}
+		if (body.get("amount") == null || body.get("amount").asText() == "") {
+			throw new ValidationException("amount should not be null");
+		}
+		if (body.get("expiry") == null || body.get("expiry").asText() == "") {
+			throw new ValidationException("expiry should not be null");
+		}
 		
-//		Object[] shareSecret = (Object[])joinPoint.getArgs();
-//		Offer offer = (Offer) shareSecret[0];
-//		
-//		if (offer.getAmount() == 0) {
-//			throw new ValidationException("Amount is missing which is the required parameter");
-//		}
-//		if (offer.getSourceCountry() == null ) {
-//			throw new ValidationException("Source Country is missing which is the required parameter");
-//		}
-//		if (offer.getSourceCurrency() == null) {
-//			throw new ValidationException("Source Currency is missing which is the required parameter");
-//		}
-//		if (offer.getDestinationCountry() == null) {
-//			throw new ValidationException("Destination Country is missing which is the required parameter");
-//		}
-//		if (offer.getDestinationCurrency() == null) {
-//			throw new ValidationException("Destination Currency is missing which is the required parameter");
-//		}
-//		if (offer.getExpiry() == null) {
-//			throw new ValidationException("Expiry Date is missing which is the required parameter");
-//		}
-
 	}
 
+	@Before("execution(public * com.cmpe275.controller.ExchangeRateController.updateOffer(..))")
+	public void ValidateExchangeUpdateOffer(JoinPoint joinPoint ) {
+
+		System.out.println("hii"+ (Long) joinPoint.getArgs()[1]);
+		System.out.println("hii"+ (JsonNode) joinPoint.getArgs()[0]);
+		JsonNode body = (JsonNode) joinPoint.getArgs()[0];
+		if (body.get("destinationCountry") == null || body.get("destinationCountry").asText() == "") {
+			throw new ValidationException("Destination Country should not be null");
+		}
+		if (body.get("destinationCurrency") == null || body.get("destinationCurrency").asText() == "") {
+			throw new ValidationException("Destination Currency should not be null");
+		}
+		if (body.get("sourceCountry") == null || body.get("sourceCountry").asText() == "") {
+			throw new ValidationException("Source Country should not be null");
+		}
+		if (body.get("sourceCurrency") == null || body.get("sourceCurrency").asText() == "") {
+			throw new ValidationException("Source Currency should not be null");
+		}
+		if (body.get("amount") == null || body.get("amount").asText() == "") {
+			throw new ValidationException("amount should not be null");
+		}
+		if (body.get("expiry") == null || body.get("expiry").asText() == "") {
+			throw new ValidationException("expiry should not be null");
+		}
+		
+		
+		
+	}
+	@Before("execution(public * com.cmpe275.controller.UserController.userSignup(..))")
+	public void ValidateUserSignup(JoinPoint joinPoint ) {
+		
+		JsonNode body = (JsonNode) joinPoint.getArgs()[1];
+		System.out.print(body);
+		if (body.get("username") == null || body.get("username").asText() == "") {
+			throw new ValidationException(" username should not be null");
+		}
+		if (body.get("nickname") == null || body.get("nickname").asText() == "") {
+			throw new ValidationException("nickname should not be null");
+		}
+		if (body.get("password") == null || body.get("password").asText() == "") {
+			throw new ValidationException(" password should not be null");
+		}
+		if (body.get("signupType") == null || body.get("signupType").asText() == "") {
+			throw new ValidationException(" signupType should not be null");
+		}
+		if (body.get("isVerified") == null) {
+			throw new ValidationException("isVerified should not be null");
+		}
+	}
+
+	@Before("execution(public * com.cmpe275.controller.BankAccountController.addAccount(..))")
+	public void ValidateAddBankAccount(JoinPoint joinPoint ) {
+	    
+		if((Enum.Currency) joinPoint.getArgs()[2]==null) {
+			throw new ValidationException(" currency should not be null");
+		}
+		if((Enum.Countries) joinPoint.getArgs()[3]==null) {
+			throw new ValidationException(" country should not be null");
+		}
+		
+	}
 
 }
